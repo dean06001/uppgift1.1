ec9ea3b3b7f83e1ed58113d529aaeb8a3254cb64
dean06001 <dean06001@E-PW0234KC>
2023-09-06 10:16:49 +0200

    Add project files.

---
+++ uppgift1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace uppgift1_1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+        }
+    }
+}
